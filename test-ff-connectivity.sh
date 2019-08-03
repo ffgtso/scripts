c921ed8c124f2c0528b770e62e3c0373aa679dee
wusel42 <wusel+src@uu.org>
2019-08-03 02:00:14 +0200

    Parallelized ;)

--- test-ff-connectivity.sh
+++ test-ff-connectivity.sh
@@ -2,37 +2,66 @@
 #
 # Supposed to run from a VM that's connected via ensX to a
 # Gluon-VM. You need a passwordless ssh key installed on the
-# Gluon-VM, it's br-client's link-local IPv6 address.
-#
-# Run via cron, e. g. ...
-#
-# * * * * * /root/test-ff-connectivity.sh >>/var/tmp/ff.log
-#
-# Have fun ;)
+# Gluon-VM, it's br-client's link-local IPv6 address. Have
+# fun ;)
 #
 GW=$(/sbin/ip -4 route show | /usr/bin/awk '/^default/ {if($2=="via") {printf("%s", $3);}}')
 NOW="$(date +'%Y-%m-%d %H:%M:%S')"
 TMPFILE=/var/tmp/$$.tmp
-KEYFILE="-i .ssh/YourPrivKey"
-GWLLADDR="fe80::dcca:fbff:fead:4201%ens8"
+KEYFILE="-i .ssh/UU-FF"
+GWLLADDR="fe80::37ff:feb0:bd4d%ens3"
+MAXWAIT=90
 
-/bin/ping -c 10 -q 1.1.1.1 > ${TMPFILE} 2>&1
-extrc=$?
-echo >> ${TMPFILE}
+echo -n > ${TMPFILE}
+
+(echo -n "$(date +'%H:%M:%S') - " > ${TMPFILE}-pingext; /bin/ping -c 10 -q 1.1.1.1 >> ${TMPFILE}-pingext 2>&1; echo $? > ${TMPFILE}-pingext-rc) & 2>/dev/null
 if [ -n "$GW" ]; then
- /bin/ping -c 10 -q "$GW" >> ${TMPFILE} 2>&1
- defrc=$?
-  echo >> ${TMPFILE}
+ (echo -n "$(date +'%H:%M:%S') - " > ${TMPFILE}-pingdef; /bin/ping -c 10 -q "$GW" >> ${TMPFILE}-pingdef 2>&1; echo $? > ${TMPFILE}-pingdef-rc) & 2>/dev/null
 fi
 
-if [ $extrc -eq 0 ]; then
+echo "$(date +'%H:%M:%S') - Batman status:" > ${TMPFILE}-batman
+/usr/bin/ssh ${KEYFILE} ${GWLLADDR} batctl gwl >${TMPFILE}-2
+gws=$(sed -e 's/^=> /   /g' -e 's/^\* /  /g' <${TMPFILE}-2 | awk '/Bit/ {printf("%s ", $1);}')
+cat ${TMPFILE}-2 >> ${TMPFILE}-batman
+/bin/rm ${TMPFILE}-2
+echo "${gws}" | /bin/grep : >/dev/null
+if [ $? -eq 0 ]; then
+ for i in ${gws}
+ do
+  (echo -n "$(date +'%H:%M:%S') - " > ${TMPFILE}-batman-${i}; /usr/bin/ssh ${KEYFILE} ${GWLLADDR} echo \; batctl tr $i >> ${TMPFILE}-batman-${i}; echo $? > ${TMPFILE}-batman-${i}-rc) & 2>/dev/null
+ done
+
+ for i in ${gws}
+ do
+  cnt=0
+  while [ ! -e ${TMPFILE}-batman-${i}-rc ]; do sleep 1; cnt=$(expr $cnt + 1 ); if [ $cnt -gt $MAXWAIT ]; then echo "TIMEOUT batman trace ${i}" >> ${TMPFILE}-batman-${i}; fi; done
+ done
+
+ for i in ${gws}
+ do
+  cat ${TMPFILE}-batman-${i} >>${TMPFILE}-batman
+ done
+fi
+
+cnt=0
+while [ ! -e ${TMPFILE}-pingext-rc ]; do sleep 1; cnt=$(expr $cnt + 1 ); if [ $cnt -gt $MAXWAIT ]; then echo "TIMEOUT ping ext"; fi; done
+if [ -n "$GW" ]; then
+ cnt=0
+ while [ ! -e ${TMPFILE}-pingdef-rc ]; do sleep 1; cnt=$(expr $cnt + 1 ); if [ $cnt -gt $MAXWAIT ]; then echo "TIMEOUT ping int"; fi; done
+fi
+
+if [ -n "$GW" ]; then
+ defrc="$(cat ${TMPFILE}-pingdef-rc)"
+fi
+
+if [ $extrc = "0" ]; then
  echo -n "${NOW}: EXT. OK   " >> ${TMPFILE}
 else
  echo -n "${NOW}: EXT. FAIL " >> ${TMPFILE}
 fi
 
 if [ -n "$GW" ]; then
- if [ $defrc -eq 0 ]; then
+ if [ $defrc = "0" ]; then
   echo -n "/ DEFAULT OK    " >> ${TMPFILE}
  else
   echo -n "/ DEFAULT FAIL  " >> ${TMPFILE}
@@ -41,13 +70,17 @@ else
  echo -n "/ DEFAULT MISSING " >> ${TMPFILE}
 fi
 echo >> ${TMPFILE}
+
 echo >> ${TMPFILE}
-echo "Gateway status:" >> ${TMPFILE}
-/usr/bin/ssh ${KEYFILE} ${GWLLADDR} batctl gwl >${TMPFILE}-2
-gws=$(sed -e 's/^=> /   /g' <${TMPFILE}-2 | awk '/Bit/ {printf("%s ", $1);}')
-cat ${TMPFILE}-2 >> ${TMPFILE}
-/bin/rm ${TMPFILE}-2
-echo "${gws}" | /bin/grep : >/dev/null && for i in ${gws} ; do /usr/bin/ssh ${KEYFILE} ${GWLLADDR} echo \; batctl tr $i >> ${TMPFILE} ; done
+cat ${TMPFILE}-pingext >> ${TMPFILE}
+
+if [ -n "$GW" ]; then
+ echo >> ${TMPFILE}
+ cat ${TMPFILE}-pingdef >> ${TMPFILE}
+fi
+
+echo >> ${TMPFILE}
+cat ${TMPFILE}-batman >> ${TMPFILE}
 echo -e "\n==========\n" >> ${TMPFILE}
 cat ${TMPFILE}
-/bin/rm ${TMPFILE}
+/bin/rm ${TMPFILE} ${TMPFILE}-*
